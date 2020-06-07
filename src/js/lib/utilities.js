279e0079db8f14a2d72e4def45330f5f8ba4672f
bhdzllr <bhdzllr@users.noreply.github.com>
2020-06-07 20:53:00 +0200

    Add JS function for disabling analytics

--- src/js/lib/utilities.js
+++ src/js/lib/utilities.js
@@ -158,14 +158,21 @@ export function beautifyFileInputs(i18n) {
 }
 
 export function addAnalyticsCode(codeCallback) {
-	var doNotTrack = (navigator.doNotTrack && (navigator.doNotTrack == '1' || navigator.doNotTrack == 'yes')) || (window.doNotTrack && (window.doNotTrack == '1')) || (navigator.msDoNotTrack && (navigator.msDoNotTrack == '1'));
-	var disableStr = 'disable-analytics';
+	const doNotTrack = (navigator.doNotTrack && (navigator.doNotTrack == '1' || navigator.doNotTrack == 'yes')) || (window.doNotTrack && (window.doNotTrack == '1')) || (navigator.msDoNotTrack && (navigator.msDoNotTrack == '1'));
+	const disableStr = 'disable-analytics';
 
 	if (doNotTrack || document.cookie.indexOf(disableStr + '=true') > -1 || document.cookie.indexOf(disableStr + '=true') > -1) {
 		window[disableStr] = true;
 	} else {
 		codeCallback();
 	}
+}
+
+export function disableAnalytics() {
+	const disableStr = 'disable-analytics';
+
+	document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+	window[disableStr] = true;
 }
 
 export function isFocusable(element) {
