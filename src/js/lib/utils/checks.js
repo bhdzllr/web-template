576b71ffde08e7ae3258dfd3b9314b32a31a7022
bhdzllr <bhdzllr@users.noreply.github.com>
2021-03-03 21:25:30 +0100

    Add JS util function for checking if an element is in viewport

--- src/js/lib/utils/checks.js
+++ src/js/lib/utils/checks.js
@@ -1,3 +1,27 @@
 export function checkJS() {
 	document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');
 }
+
+/**
+ * @see {@link https://stackoverflow.com/a/7557433/6458288}
+ * @see {@link https://stackoverflow.com/users/139361/dan}
+ */
+export function isElementInViewport(el, viewport) {
+	const rect = el.getBoundingClientRect();
+
+	if (viewport) {
+		return (
+			rect.top >= 0
+			&& rect.right <= (viewport.innerWidth || viewport.clientWidth)
+			&& rect.bottom <= (viewport.innerHeight || viewport.clientHeight)
+			&& rect.left >= 0
+		);
+	}
+
+	return (
+		rect.top >= 0
+		&& rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		&& rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+		&& rect.left >= 0
+	);
+}
