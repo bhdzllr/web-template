4ccc55210f968be66e1d1060f493722ef168aa3d
bhdzllr <bhdzllr@users.noreply.github.com>
2025-11-20 22:51:06 +0100

    Refactor local storage module

---
+++ public/js/lib/modules/local-storage.js
@@ -0,0 +1,50 @@
+export const localStorageAdapter = {
+
+	set(key, value) {
+		const insertValue = (value !== null && typeof value === 'object')
+			? JSON.stringify(value)
+			: value;
+
+		localStorage.setItem(key, insertValue);
+	},
+
+	get(key) {
+		const item = localStorage.getItem(key);
+		if (item === null) return null;
+
+		try {
+			return JSON.parse(item);
+		} catch (e) {
+			return item;
+		}
+	},
+
+	remove(key) {
+		localStorage.removeItem(key);
+	},
+
+	push(key, value) {
+		const current = this.get(key);
+
+		if (!current) {
+			current = [];
+		} else if (!Array.isArray(current)) {
+			throw new Error(`Can't push, [${key}] is not an array.`);
+		}
+
+		current.push(value);
+		this.set(key, current);
+	},
+
+	splice(key, index, splice = 1) {
+		const current = this.get(key);
+
+		if (!Array.isArray(current)) {
+			throw new Error(`Can't splice, [${key}] is not an array.`);
+		}
+
+		current.splice(index, splice);
+		this.set(key, current);
+	},
+
+};
